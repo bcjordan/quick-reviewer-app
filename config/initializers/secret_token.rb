a79defb5c9cad15387eaabd451185e8d5e96d40c
Brian Jordan <bcjordan@gmail.com>
2011-02-08 01:24:42 -0500

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeerSite::Application.config.secret_token = 'fb312c530b71863d65130f527009b5747e6dd15233ec2302828f0988eaaeed4d16185381d53c5f7dca0ecc51f66198efdd5147b51f5d72d24be0da863174ef68'
